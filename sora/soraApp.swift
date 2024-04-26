f04c5ecd15891737676ed7626634cd71a8d7aac8
fridayhoney <gotham936@gmail.com>
2024-04-27 00:26:42 +0400

    Initial Commit

---
+++ sora/soraApp.swift
@@ -0,0 +1,17 @@
+//
+//  soraApp.swift
+//  sora
+//
+//  Created by Elias CHETOUANI on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct soraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
